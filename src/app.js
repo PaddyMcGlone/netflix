06dce2f50b3dc70b730ac667a1a63072f651eff7
Paddy McGlone <paddymcglone@gmail.com>
2021-05-09 12:17:01 +0100

    Getting the basics of compound components working.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Jumbotron from './components/jumbotron';
+import JumboData from './fixtures/jumbo';
 
 export default function App() {
   return (  
-    <Jumbotron/>
+    <Jumbotron.Container>
+      {JumboData.map((item) => (
+          <Jumbotron key={item.key} direction={item.direction}>
+            <p>{item.title}</p>
+            <p>{item.subTitle}</p>
+            <p>{item.image}</p>
+            <p>{item.alt}</p>
+          </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import {Container, Inner} from './styles/jumbotron';
 
-export default function Jumbotron(){
-    return(<p>I am the jumbrotron</p>)
-}
+export default function Jumbotron({ children, direction = 'row', ... restProps }) {
+    return(
+        <Inner direction={direction}>
+            {children}
+        </Inner>
+    )}
+
+
+    Jumbotron.Container = function JumbotronContainer({ children, ...restProps}) {
+        return <Container {...restProps}>{children}</Container>
+    }
